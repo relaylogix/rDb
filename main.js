2080431f2a2c2aea35d9c1bf78f078415e904159
relaylogix <61469369+relaylogix@users.noreply.github.com>
2020-08-02 14:14:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,15 +18,29 @@ const SMK = require('@smokenetwork/smoke-js');
 // Weather library
 const WTH = require('weather-js');
 
+// Set Global Constants
+
+// Command Prefix To Use
+const PRE = ']';
+
+
 // Bot Connection message
 BOT.on('ready', () => { console.log(`${BOT.user.tag} is online.`); });
 
 // Bot Message Recieve
 BOT.on('message', msg => {
 // Start Of Command Handler
-
-
-
+  // Does The Command Start With The Prefix And Command Not Sent By Bot - Exit On Bad Condition
+  if(!msg.content.startswith(PRE) || msg.author.BOT) return;
+  
+  // Setup Command Variables
+  const args = msg.content.slice(PRE, length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
+  
+  // Start Of Command Interpretation
+  if(cmd === 'ping'){
+     msg.channel.send('Pong');
+  }
 
 // End Of Command Handler
 });
