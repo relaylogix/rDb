d4f903e549e99d0e6d11855f6f55e623247e74c0
relaylogix <61469369+relaylogix@users.noreply.github.com>
2020-08-02 13:54:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,29 @@
 
 // discord library
 const DISCORD = require('discord.js');
-
+// Set New Discord Client
 const BOT = new DISCORD.Client();
 
-BOT.on('ready', () => { console.log(`${BOT.user.tag} is online.`); });
-BOT.on('message', msg => {
+// FileSystem library
+const FS = require('fs');
 
+// Smoke Library
+const SMK = require('@smokenetwork/smoke-js');
+
+// Weather library
+const WTH = require('weather-js');
+
+// Bot Connection message
+BOT.on('ready', () => { console.log(`${BOT.user.tag} is online.`); });
+
+// Bot Message Recieve
+BOT.on('message', msg => {
+// Start Of Command Handler
+
+
+
+
+// End Of Command Handler
 });
 
 // Initial Bot Logon
