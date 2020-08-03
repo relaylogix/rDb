87b0a2531876a348ab212317e75ea599d12c6914
relaylogix <61469369+relaylogix@users.noreply.github.com>
2020-08-02 22:00:57 -0500

    Update get_wit.js

--- cmd/get_wit.js
+++ cmd/get_wit.js
@@ -1,16 +1,16 @@
 module.exports = {
    name: 'gwc',
-   desription: "This is the first command interaction with the smoke-js library.",
+   desription: "This command returns the alltime smoke network registered witness count.",
    execute(message, args){
-   
+
         // Load Smoke Library
         const SMK = require('@smokenetwork/smoke-js');
-        
+
         SMK.api.getWitnessCount(function(err, result) {
-          console.log(err, result);
+                if(!err){
+                console.log(`Witness Count Alltime: ${result}`);
+                message.channel.send(`Since the beginning, the total Smoke Network witnesses count is: ${result}`);
+                }
         });
-        
-        
-        message.channel.send('https://smoke.io');
-   }
+        }
 }
